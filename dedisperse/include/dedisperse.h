3703e549c4e302e07ff98ac1364d58fa6201a90b
Thomas <tjstolk@yahoo.com>
2019-05-23 23:22:38 +0200

    Fixed typo in dedispersion.h

--- dedisperse/include/dedisperse.h
+++ dedisperse/include/dedisperse.h
@@ -6,7 +6,7 @@
 #include "linspaced.h"
 
 
-void dedisperse(filterbank& fb, double max_delay, float dispersion_measure, uint32_t highest_x);
+void dedisperse(filterbank& fb, float dispersion_measure);
 std::pair<uint32_t, uint32_t> find_line(filterbank* fb, uint32_t start_sample, double max_delay, float pulsar_intensity);
 float find_estimation_intensity(filterbank& fb, uint32_t highest_x);
 float find_dispersion_measure(filterbank& fb, float pulsar_intensity, double max_delay);
